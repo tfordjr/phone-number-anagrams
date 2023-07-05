ca1c3a93facc36feee5e8b34d742796d3fc5bbdb
tfordjr <98005445+tfordjr@users.noreply.github.com>
2023-07-05 12:33:46 -0500

    Switched to switch statement, added rewrite & append

--- encrypted-file/encrypted-file.py
+++ encrypted-file/encrypted-file.py
@@ -10,9 +10,10 @@ EDITOR = os.environ.get('EDITOR', 'vim')
 
 def menu():
     userInput = 0
-    print("You can...\n\t1) Read settings from a settings file.\n\t2) Modify the settings in the settings file.\nWhat would you like to do?: ", end="")
+    print("You can...\n\t1) Read settings from the settings file.\n\t2) Append to the settings file.\n\t3) Delete and rewrite the settings file.\n\t4) Quit the program")
     while userInput != 1 and userInput != 2 and userInput != 3 and userInput != 4:
         try:
+            print("What would you like to do?: ", end="")
             userInput = int(input())
         except ValueError:
             print("Please enter 1 or 2.")
@@ -27,35 +28,33 @@ def decode(ciphertext):
     #print("decoding ", ciphertext)
 
 def main():
-    f = open(r"C:\Users\Terry\All Projects\encryptedSettings\settings.txt", "r+")
+    f = open(r"C:\Users\Terry\All Projects\encrypted-file\settings.txt", "r")
     print(f.read())
     ciphertext = encode("File Contents & save to settings") 
     while True:
-        if menu() == 1:     
-            f = open(r"C:\Users\Terry\All Projects\encryptedSettings\settings.txt", "r")       
-            print(f.read())
-            decode(ciphertext) # and print plaintext
-        else:
-            #f = open(r"C:\Users\Terry\All Projects\encryptedSettings\settings.txt", "r+")
+        match menu():
+            case 1:    # read   
+                f = open(r"C:\Users\Terry\All Projects\encrypted-file\settings.txt", "r")       
+                print("\nDecypting and printing settings.txt")
+                print(f.read())
+                #decode(ciphertext) # and print plaintext
+            case 2:    # append
+                print("Appending settings.txt")
+                #f = open(r"C:\Users\Terry\All Projects\encrypted-file\settings.txt", "r+")
+            case 3:    # rewrite
+                print("Rewriting settings.txt")
+                #f = open(r"C:\Users\Terry\All Projects\encrypted-file\settings.txt", "r+")
+            case 4:    # quit
+                print("Quitting the program...")
+                quit()
+            case default:
+                pass            
                         
-            #f.flush()
+                        
             
-            initial_message = b"" # if you want to set up the file somehow
-
-            tf = r"C:\Users\Terry\All Projects\encryptedSettings\settings.txt"
-            tf.write(initial_message)
-            tf.flush()
-            call([EDITOR, tf.name])
-
-            tf.seek(0)
-            edited_message = tf.read()
-            print (edited_message.decode("utf-8"))
-                
+                            
 
 
-            #call(['EDITOR', r"C:\Users\Terry\All Projects\encryptedSettings\settings.txt"])
-
-            decode(ciphertext) # save back to file and let 
-            # user edit file and after he's done encode()
+            
 
 main()
