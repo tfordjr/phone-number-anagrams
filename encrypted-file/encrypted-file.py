cfd4dbb890c2534c987458a5b8710d236b720e14
tfordjr <98005445+tfordjr@users.noreply.github.com>
2023-07-09 12:11:19 -0500

    AES basics working, writing encyrpted data to file

--- encrypted-file/encrypted-file.py
+++ encrypted-file/encrypted-file.py
@@ -1,7 +1,48 @@
-import cryptography
-import random
-# from Crypto.Cipher import AES
-# from Crypto.Random import get_random_bytes
+# import cryptography
+# import random
+# import hashlib
+# from Crypto import Random
+
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+# key = get_random_bytes(32)
+# print(key)
+
+salt = b'\xc2b\xa1y\xe9 0\x83K4\xbd\xff\x8c&]\x84\xf6L\xc6\xac\x03\xfa\xbdT\xef\xb8\xa6LM\xe8\xb9^'
+password = "mypassword"
+
+key = PBKDF2(password, salt, dkLen=32)
+
+message = b"German Shepherd, 120, 3.4"
+
+cipher = AES.new(key, AES.MODE_CBC)
+ciphered_data = cipher.encrypt(pad(message, AES.block_size))
+
+with open('settings.txt', 'wb') as f:
+    f.write(cipher.iv)
+    f.write(ciphered_data)
+
+with open('settings.txt', 'rb') as f:
+    iv = f.read(16)
+    decrypt_data = f.read()
+
+cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
+print(original)
+
+# def encode(plaintext):
+
+#     pass
+
+    
+#     #print("encoding ", plaintext)
+
+# def decode(ciphertext):
+#     pass
+#     #print("decoding ", ciphertext)
 
 def menu():
     userInput = 0
@@ -13,17 +54,6 @@ def menu():
         except ValueError:
             print("Please enter 1-4.")
     return userInput
-
-def encode(plaintext):
-
-    pass
-
-    
-    #print("encoding ", plaintext)
-
-def decode(ciphertext):
-    pass
-    #print("decoding ", ciphertext)
 
 def main():
     f = open(r"C:\Users\Terry\All Projects\encrypted-file\settings.txt", "r")
@@ -54,4 +84,4 @@ def main():
             case default:
                 pass 
             
-main()
+#main()
